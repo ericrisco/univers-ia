acc8e17a4417acb63bbf89b674bb0266f17cd920
Eric Risco de la Torre <erisco@icloud.com>
2024-08-03 02:54:05 +0200

    feat: Add inference router to handle inference requests

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 import atlastMongodbConnect from './db/connector.js';
 import general from './routers/general.js';
+import inference from './routers/inference.js';
+
 import { ingestion } from './loaders/document_loader.js';
 import { query } from './services/document_query.js';
 
@@ -19,6 +21,7 @@ app.use(morgan('[:date[clf]]: :remote-addr ":user-agent" :method :url :status - 
 
 const mainUrl = '/univers-ia/api/v1';
 app.use(mainUrl, general);
+app.use(mainUrl, inference);
 
 app.listen(PORT, async () => {
 	console.log('Univers IA API is listening on port:', PORT);
